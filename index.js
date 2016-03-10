c714e01f9b4b2a421395ec8c347e7b6daea7a4cc
Dmitriy Velychko <d.velychko@disclab.com>
2016-03-10 13:17:19 +0200

    Refactoring index.js

--- index.js
+++ index.js
@@ -2,10 +2,56 @@ var request = require("request");
 var fs = require("fs");
 var async = require("async");
 
+var bitrixConfig = {};
+
+var writeRequest = function (response, body, callback) {
+    var answer = JSON.parse(body);
+
+    fs.writeFile(bitrixConfig.path, JSON.stringify({
+        domain: bitrixConfig.domain,
+        grantType: bitrixConfig.grantType,
+        clientId: bitrixConfig.clientId,
+        clientSecret: bitrixConfig.clientSecret,
+        scope: bitrixConfig.scope,
+        redirectURL: bitrixConfig.redirectURL,
+        refreshToken: answer.refresh_token,
+        path: bitrixConfig.path
+    }), function (err) {
+        if (err) return console.error(err);
+    });
+
+    bitrixConfig.accessToken = answer.access_token;
+    bitrixConfig.refreshToken = answer.refresh_token;
+
+    if (typeof callback === 'function') {
+        return callback();
+    }
+};
+
+var httpRequest = function (callback) {
+    request(bitrixConfig.domain + "/oauth/token/?grant_type=" + bitrixConfig.grantType +
+        "&client_id=" + bitrixConfig.clientId + "&client_secret=" + bitrixConfig.clientSecret +
+        "&refresh_token=" + bitrixConfig.refreshToken + "&scope=" + bitrixConfig.scope +
+        "&redirect_uri=" + bitrixConfig.redirectURL, function (error, response, body) {
+        callback(error, response, body)
+    })
+};
+
+var requestToken = function () {
+    async.waterfall([
+        httpRequest,
+        writeRequest
+    ]);
+};
+
+var updateAccessToken = function (req, res, next) {
+    req.accessToken = bitrixConfig.accessToken;
+    next();
+};
 
 var bitrixAccessToken = function (options) {
 
-    var bitrixConfig = options;
+    bitrixConfig = options;
 
     if (!bitrixConfig.domain || !bitrixConfig.grantType || !bitrixConfig.clientId || !bitrixConfig.clientSecret || !bitrixConfig.scope || !bitrixConfig.redirectURL || !bitrixConfig.refreshToken || !bitrixConfig.path) {
         throw Error('Define properties for bitrix access');
@@ -13,55 +59,8 @@ var bitrixAccessToken = function (options) {
 
     bitrixConfig.accessToken = '';
 
-    var requestToken = function (options) {
-
-        var httpRequest = function (options, callback) {
-            request(options.domain + "/oauth/token/?grant_type=" + options.grantType +
-                "&client_id=" + options.clientId + "&client_secret=" + options.clientSecret +
-                "&refresh_token=" + options.refreshToken + "&scope=" + options.scope +
-                "&redirect_uri=" + options.redirectURL, function (error, response, body) {
-                callback(error, response, body)
-            })
-        };
-
-        var writeRequest = function (response, body, callback) {
-            var answer = JSON.parse(body);
-
-            fs.writeFile(options.path, JSON.stringify({
-                domain: options.domain,
-                grantType: options.grantType,
-                clientId: options.clientId,
-                clientSecret: options.clientSecret,
-                scope: options.scope,
-                redirectURL: options.redirectURL,
-                refreshToken: answer.refresh_token,
-                path: options.path
-            }), function (err) {
-                if (err) return console.log(err);
-            });
-
-            bitrixConfig.accessToken = answer.access_token;
-            bitrixConfig.refreshToken = answer.refresh_token;
-
-            if (typeof callback === 'function') {
-                return callback();
-            }
-        };
-
-        async.waterfall([
-            httpRequest.bind(null, options),
-            writeRequest
-        ], function (err, result) {
-        });
-    };
-
-    var updateAccessToken = function (req, res, next) {
-        req.accessToken = bitrixConfig.accessToken;
-        next();
-    };
-
-    setInterval(requestToken.bind(null, bitrixConfig), 1000*60*59);
-    requestToken(bitrixConfig);
+    setInterval(requestToken, 1000*60*59);
+    requestToken();
 
     return updateAccessToken;
 };
